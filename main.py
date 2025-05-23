66aa32b8143a38e849732ec7025310a7a9b5ede6
sub-regmi <83897492+sub-regmi@users.noreply.github.com>
2025-05-23 14:55:20 +0545

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 )
 
 # ====== CONFIG ======
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 HUGGINGFACE_API_KEY = "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"  # <== Replace with your Hugging Face token
